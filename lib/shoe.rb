825435cbe9eb186fe3e25aac036ef06c4150477b
Benjamin Addai <addai.5@buckeyemail.osu.edu>
2018-11-26 02:36:28 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,11 +7,6 @@ class Shoe
   def initialize(brand)
     @brand = brand
   end
-
-  def cobble
-    self.condition = "new"
-    puts "Your shoe is as good as new!"
-  end
   
   def brand=(brand)
     @brand = brand
