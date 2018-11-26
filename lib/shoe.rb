d5311e24eb768ac544bc706e12fc19702bebdc42
Benjamin Addai <addai.5@buckeyemail.osu.edu>
2018-11-26 02:37:13 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,8 @@
 class Shoe
-  attr_accessor(:color, :size, :material, :condition)
-  attr_reader(:brand)
+  attr_accessor(:color, :size, :material, :condition, :brand)
+  # attr_reader()
 
   BRANDS = []
-  
   
   
   def brand=(brand)
